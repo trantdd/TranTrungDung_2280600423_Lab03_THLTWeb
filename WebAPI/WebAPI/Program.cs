261c6880ddce9b10d95599bd43b98412f2cb453a
0423_Trần Trung Dũng <dungtrantrung603@gmail.com>
2025-04-03 11:33:31 +0700

    done

---
+++ WebAPI/WebAPI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WebAPI.Models;
+using WebAPI.Repositories; // Add this using directive
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyAllowOrigins", policy =>
+    {
+        //Thay bằng địa chỉ localhost khi khởi chạy bên frontend (VSCode)
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+//Đặt trên UseAuthorization
+
+app.UseAuthorization();
+app.UseCors("MyAllowOrigins");
+app.MapControllers();
+app.Run();
---
+++ WebAPI/WebAPI/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
